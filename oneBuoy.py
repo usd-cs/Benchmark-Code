4b705aeb115fc1949f5c4ad7d9a3f0113f38190d
Marissa <marissanicoleesteban@sandeigo.edu>
2024-02-20 15:41:30 -0800

    one buoy

--- oneBuoy.py
+++ oneBuoy.py
@@ -19,42 +19,63 @@ from seebuoy import NDBC
 %matplotlib inline 
 from sklearn.impute import SimpleImputer
 
+def get_buoy_data(buoy_num):
+    ndbc = NDBC()
+    return ndbc.get_data(buoy_num)
 
-ndbc = NDBC()
-wave_df = ndbc.stations()
-df_data = ndbc.available_data()
-
-# Get info on NY Harbor Buoy
-station_id = "44065"
-ny_buoy = ndbc.get_data(station_id)
-
-def handle_missing_data(ny_buoy):
+def handle_missing_data(buoy):
     """
     The data has some missing values. We impute these values by mode, mean, and interpolation.
     """
-
-
     # dropping cols where there is 100% NA
-    ny_buoy.dropna(axis=1, how='all', inplace=True)
+    buoy.dropna(axis=1, how='all', inplace=True)
 
     # dropping rows where target value us null
-    ny_buoy.dropna(subset=['average_period'], inplace=True)
-    ny_buoy.dropna(subset=['wave_height'], inplace=True)
-
+    buoy.dropna(subset=['average_period'], inplace=True)
+    buoy.dropna(subset=['wave_height'], inplace=True)
 
     # IMPUTATIONS
     # Replace missing data with mode
     imputer = SimpleImputer(strategy='most_frequent')
-    imputer.fit(ny_buoy)
-    ny_buoy_mode = pd.DataFrame(imputer.transform(ny_buoy), columns=ny_buoy.columns)
+    imputer.fit(buoy)
+    buoy_mode = pd.DataFrame(imputer.transform(buoy), columns=buoy.columns)
 
     # Replace missing data with mean
     imputer = SimpleImputer(strategy='mean')
-    imputer.fit(ny_buoy)
-    ny_buoy_mean = pd.DataFrame(imputer.transform(ny_buoy), columns=ny_buoy.columns)
+    imputer.fit(buoy)
+    buoy_mean = pd.DataFrame(imputer.transform(buoy), columns=buoy.columns)
 
     # Interpolate missing values using spline interpolation
-    ny_buoy_interpolated = ny_buoy.interpolate(method='spline', order=2)
+    buoy_interpolated = buoy.interpolate(method='spline', order=2)
 
-    return (ny_buoy_mean, ny_buoy_mode, ny_buoy_interpolated)
+    return (buoy_mean, buoy_mode, buoy_interpolated)
 
+
+
+def handle_outliers(buoy):
+    """
+    """
+
+    # Remove non finite values
+    ny_buoy_mode = ny_buoy_mode[np.isfinite(ny_buoy_mode['wave_height'])]
+    ny_buoy_mean = ny_buoy_mean[np.isfinite(ny_buoy_mean['wave_height'])]
+    ny_buoy_interpolated = ny_buoy_interpolated[np.isfinite(ny_buoy_interpolated['wave_height'])]
+
+
+
+def handle_analytics(buoy, model):
+    """
+    """
+
+    train_df = ny_buoy[ny_buoy_mode.columns.difference(['wave_height', 'average_period'])]
+    
+    # Validation function for wave height
+    n_folds = 5
+
+    def rmse_cv(model,n_folds):
+        kf=KFold(n_splits=n_folds)
+        rmse = np.sqrt(-cross_val_score(model, train_df, ny_buoy_mode.wave_height, scoring="neg_mean_squared_error", cv = kf))
+        return rmse
+
+
+buoy = get_buoy_data("44065")
